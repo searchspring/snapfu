2d90a3b48d427f8052a4f4411a5e76b48e5f4177
kevin <kevin@searchspring.com>
2021-06-11 15:54:10 -0600

    Updating about

--- src/about.js
+++ src/about.js
@@ -2,6 +2,11 @@ import chalk from 'chalk';
 import packageJSON from '../package.json';
 
 export const about = (options) => {
-	console.log(`${chalk.blue('snap · fu - "the way of snap"')} ${chalk.red(`(v${packageJSON.version})`)}
-${chalk.cyan('https://github.com/searchspring/snapfu')}`);
+	console.log(`${chalk.blue('snap · fu -')} ${chalk.blue.italic('"the way of snap"')} ${chalk.green(`(v${packageJSON.version})`)}\n`);
+	console.log(`${chalk.blue('──────────────────────────────────────')}\n`);
+	// console.log(`${chalk.blue('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')}`)
+	console.log(`${chalk.blue('https://github.com/searchspring/snap')}\n`);
+	// console.log(`${chalk.blue('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')}\n`)
+	console.log(`${chalk.blue('──────────────────────────────────────')}\n`);
+	console.log(`${chalk.cyan('Searchspring')} ${chalk.red('❤')} ${chalk.cyan('2021')}`);
 };
