ef98008a051b931bf397d26d7a37259b3018edf4
kevin <kevin@searchspring.com>
2022-07-13 12:48:11 -0600

    refactor(style): changed chalk colors to more standardized colors

--- src/about.js
+++ src/about.js
@@ -2,9 +2,7 @@ import chalk from 'chalk';
 
 export const about = (options) => {
 	console.log(
-		`${chalk.greenBright('snap · fu')} ${chalk.grey(' ~ ')} ${chalk.green.italic('"the way of snap"')}  ${chalk.red(
-			`(v${options.context.version})`
-		)}\n`
+		`${chalk.cyanBright('snap · fu')} ${chalk.grey(' ~ ')} ${chalk.cyan.italic('"the way of snap"')}  ${chalk.red(`(v${options.context.version})`)}\n`
 	);
 	console.log(`${chalk.grey('─────────────────────────────────────────────')}\n`);
 	console.log(`${chalk.bold.grey('https://github.com/searchspring/snap')}`);
